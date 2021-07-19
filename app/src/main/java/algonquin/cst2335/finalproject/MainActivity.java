f1049d8cf2057641f4eebe5163e486344a26a263
chi zhang <zhan0627@algonquinlive.com>
2021-07-19 02:26:04 -0400

    team work

--- app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
@@ -1,8 +1,10 @@
 package algonquin.cst2335.finalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView mytext = findViewById(R.id.textview);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+
+        button1.setOnClickListener(click -> {});
+        button2.setOnClickListener(click -> {});
+        button3.setOnClickListener(click -> {});
+        button4.setOnClickListener(click -> {});
+
+
     }
 }
