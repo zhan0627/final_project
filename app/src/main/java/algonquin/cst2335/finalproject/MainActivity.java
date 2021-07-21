99f481593f81d8174ae912459da3662252516cad
Jace <omad0008@algonquinlive.com>
2021-07-21 12:58:58 -0400

    finished milestone1

--- app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/finalproject/MainActivity.java
@@ -1,28 +1,136 @@
-package algonquin.cst2335.finalproject;
+package algonquin.cst2335.jacesandroidlabs;
 
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
-
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "MainActivity";
+    EditText et;
+    Bitmap mBitmap;
+    private Object SecondActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView mytext = findViewById(R.id.textview);
-        Button button1 = findViewById(R.id.button1);
-        Button button2 = findViewById(R.id.button2);
-        Button button3 = findViewById(R.id.button3);
-        Button button4 = findViewById(R.id.button4);
 
-        /*button1.setOnClickListener(click -> {});
-        button2.setOnClickListener(click -> {});
-        button3.setOnClickListener(click -> {});
-        button4.setOnClickListener(click -> {});*/
+        Button loginBtn = findViewById(R.id.loginButton);
+        Log.w("MainActivity", "In onCreate() - Loading Widgets" );
+
+        loginBtn.setOnClickListener( clk -> {
+            et = findViewById(R.id.inputEditText);
+            Intent nextPage = new Intent(MainActivity.this, algonquin.cst2335.jacesandroidlabs.SecondActivity.class);
+
+            nextPage.putExtra("EmailAddress", et.getText().toString());
+            nextPage.putExtra("SomeInfo", "Welcome!");
+            nextPage.putExtra("MyFloat", 3.14f);
+
+            startActivityForResult(nextPage, 900);
+        });
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 900)
+        {
+            if(resultCode == RESULT_OK) {
+                Bitmap thumbnail = data.getParcelableExtra("data");
+                FileOutputStream fOut = null;
+                try {
+                    fOut = openFileOutput( "Picture.png", Context.MODE_PRIVATE);
+                    thumbnail.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                    fOut.flush();
+                    fOut.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+
+                }
+            }
+
+
+
+            Log.w( "MainActivity", "Coming back from previous" );
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+
+        String TAG = "MainActivity";
+        Log.d( TAG, "The application is now visible on screen");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+
+        String TAG = "MainActivity";
+        Log.d( TAG, "The application is no longer visible.");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+
+        String TAG = "MainActivity";
+        Log.d( TAG, "Any memory used by the application is freed.");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+
+        String TAG = "MainActivity";
+        Log.d( TAG, "he application is restarting.");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+
+        String TAG = "MainActivity";
+        Log.d( TAG, "The application is now responding on user input");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+
+        String TAG = "MainActivity";
+        Log.d( TAG, "The application no longer responds to user input");
     }
 }
