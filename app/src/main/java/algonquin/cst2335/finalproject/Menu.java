a5acf3a9936c93329ca96930f54907e0eb7c2fdf
Jace <omad0008@algonquinlive.com>
2021-08-11 09:58:22 -0400

    finished milestone2

--- app/src/main/java/algonquin/cst2335/finalproject/Menu.java
+++ app/src/main/java/algonquin/cst2335/finalproject/Menu.java
@@ -2,14 +2,19 @@ package algonquin.cst2335.finalproject;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
 import java.io.FileOutputStream;
 
 public class Menu extends AppCompatActivity {
@@ -17,7 +22,8 @@ public class Menu extends AppCompatActivity {
     private static String TAG = "MainActivity";
     EditText et;
     Bitmap mBitmap;
-    private Object SecondActivity2;
+    private Object SecondActivity;
+    ImageView pict;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,13 +52,43 @@ public class Menu extends AppCompatActivity {
 
         btn.setOnClickListener( clk -> {
             Intent sendBack = new Intent ();
-            sendBack.putExtra("Number", line.getText().toString());
+            sendBack.putExtra("search", line.getText().toString());
             setResult(200, sendBack);
 
             finish();
         });
 
+        String sandbox = getFilesDir().getPath();
+        File file = new File( sandbox + "/Picture.png");
+        if(file.exists())
+        {
+            Bitmap theImage = BitmapFactory.decodeFile(sandbox+"/Picture.png");
+            pict = findViewById(R.id.imageView);
+            pict.setImageBitmap( theImage );
+        }
 
+        Button callBtn = findViewById(R.id.button2);
+        Log.w("MainActivity", "In onCreate() - Loading Widgets" );
+
+        callBtn.setOnClickListener( clk -> {
+
+            Intent movie = new Intent(Intent.ACTION_WEB_SEARCH);
+            movie.setData(Uri.parse("tel:" + line.getText().toString()));
+            //save line.getText().toString() to sharedpreferences
+            SharedPreferences prefs = getSharedPreferences("MySearch", Context.MODE_PRIVATE);
+
+            String called = getFilesDir().getPath();
+            File files = new File( called +"/shared_prefs");
+            if(file.exists())
+            {
+                prefs.getString("MySearch", line.getText().toString());
+                String mySearch = prefs.getString("MySearch", line.getText().toString());
+                SharedPreferences.Editor  editor = prefs.edit();
+                editor.putString("MySearch", mySearch);
+                editor.apply();
+            }
+            startActivityForResult(movie, 1233);
+        });
     }
 
     @Override
